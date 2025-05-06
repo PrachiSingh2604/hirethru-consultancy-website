cfa2b68dbfb35a3e92feb4d279fa3665389840c2
PrachiSingh2604 <prachi.singh1624@gmail.com>
2025-05-07 02:16:03 +0530

    --

--- hirethru-consultancy-website/src/App.js
+++ hirethru-consultancy-website/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { AppBar, Toolbar, Box } from '@mui/material';
+import HeroSection from './components/HeroSection';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <AppBar position="static" sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
+        <Toolbar>
+          <img src="/assets/logo.png" alt="Hirethru Logo" style={{ height: 50 }} />
+        </Toolbar>
+      </AppBar>
+      <HeroSection />
+    </Box>
   );
 }
 
